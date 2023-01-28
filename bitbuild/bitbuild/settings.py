cf994873add6d469e6b9e3baae89554fed99c6b5
Harsh Rane <hharshup@gmail.com>
2023-01-29 05:26:58 +0530

    mario theme

--- bitbuild/bitbuild/settings.py
+++ bitbuild/bitbuild/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-from decouple import config
+#from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-@d$cl%ir86*3y2%8z(%@g*=3t13$9tvb5_40fjx4t81xv0g90l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
